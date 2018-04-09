82f27699697fe4520f511e4076efecb255f8a78b
Cody <meanderingmonarch@gmail.com>
2018-04-09 09:10:05 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <ContactList />
+        </div>
+    );
+  }
+}
+
+export default App;
